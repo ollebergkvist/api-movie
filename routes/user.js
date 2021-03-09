893caae380aa679e4e028090889bd5ef6bff1a9a
Olle Bergkvist <ollebergkvist@gmail.com>
2021-03-09 13:00:23 -0700

    Added validator to user routes

--- routes/user.js
+++ routes/user.js
@@ -1,25 +1,47 @@
 const express = require('express'); // Express module
 const router = express.Router(); // Bind express method to router
+const validator = require('express-joi-validation').createValidator({}); // Middleware to validate req.body, req.params and req.query
 const userController = require('../controllers/user.js'); // Route handler for users
 const auth = require('../middleware/auth.js'); // Middleware to authenticate with JWT
 const admin = require('../middleware/admin.js'); // Middleware to verify admin rights
 
 // Update user permissions
-router.put('/user/:id', auth, admin, userController.updateUserRights);
+router.put(
+	'/user/:id',
+	auth,
+	admin,
+	validator.query(schemas.id),
+	userController.updateUserRights
+);
 
 // Get all users
 router.get('/users', auth, admin, userController.getUsers);
 
 // Get user by id
-router.post('/user/:id', auth, admin, userController.getUser);
+router.post(
+	'/user/:id',
+	auth,
+	admin,
+	validator.query(schemas.id),
+	userController.getUser
+);
 
 // Register user
-router.post('/register', userController.register);
+router.post(
+	'/register',
+	validator.body(schemas.register),
+	userController.register
+);
 
 // Login
-router.post('/login', userController.login);
+router.post('/login', validator.body(schemas.login), userController.login);
 
 // Like movie
-router.put('/favorite/:id', auth, userController.addFavoriteMovie);
+router.put(
+	'/favorite/:id',
+	auth,
+	validator.query(schemas.id),
+	userController.addFavoriteMovie
+);
 
 module.exports = router;
