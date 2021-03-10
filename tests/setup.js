4c6c514ea6ba3587913c9de2be638bd8c7419e54
Olle Bergkvist <ollebergkvist@gmail.com>
2021-03-10 10:47:02 -0700

    Updated db info in jest setup.js file

--- tests/setup.js
+++ tests/setup.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 beforeEach((done) => {
 	mongoose.connect(
-		`mongodb://localhost:27017/${process.env.TEST_SUITE}`,
+		`mongodb://localhost:27017/movies`,
 		{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
 		() => done()
 	);
