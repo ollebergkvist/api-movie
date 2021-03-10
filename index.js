c8d64f381ac079a3fd60dfd9d8cd36c8885641ac
Olle Bergkvist <ollebergkvist@gmail.com>
2021-03-10 09:45:14 -0700

    Fixed .env bug so that deployment with heroku works

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const createServer = require('./models/server.js');
-var port = 3000;
-var uri =
-	'mongodb+srv://ollebergkvist:76yCT7fyEA8TNcJB@cluster0.bhxhf.mongodb.net/movies?retryWrites=true&w=majority';
 
-// if (process.env.NODE_ENV === 'test') {
-// 	uri = 'mongodb://localhost:27017/movies';
-// 	port = 3000;
-// } else {
-// 	uri = process.env.MONGDODB_URI;
-// 	port = process.env.PORT;
-// }
+if (process.env.NODE_ENV === 'test') {
+	uri = 'mongodb://localhost:27017/movies';
+	port = 3000;
+} else {
+	uri = process.env.MONGODB_URI;
+	port = process.env.PORT;
+}
 
 const connection = mongoose
 	.connect(uri, {
